886024a6a74abe51ee8861c82b3cb90bd6db3362
TUTSUNOM-230390\tutsunom <tomoki.utsunomiya@oracle.com>
2026-01-14 13:57:49 +0900

    comment out some vars

--- providers.tf
+++ providers.tf
@@ -6,9 +6,9 @@ terraform {
 provider "oci" {
   version          = ">= 3.0.0"
   tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  #user_ocid        = var.user_ocid
+  #fingerprint      = var.fingerprint
+  #private_key_path = var.private_key_path
   region           = var.region
 }
 
