d1a6ac45bc29eefbf8d57397620839c0639f858a
TUTSUNOM-230390\tutsunom <tomoki.utsunomiya@oracle.com>
2026-01-14 13:59:33 +0900

    comment out tenancy_ocid

--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ terraform {
 # ------ Assign OCI Provider
 provider "oci" {
   version          = ">= 3.0.0"
-  tenancy_ocid     = var.tenancy_ocid
+  #tenancy_ocid     = var.tenancy_ocid
   #user_ocid        = var.user_ocid
   #fingerprint      = var.fingerprint
   #private_key_path = var.private_key_path
