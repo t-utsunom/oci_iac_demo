0dc06cf2f15ec862d64ed8fabf412ceb2d817727
TUTSUNOM-230390\tutsunom <tomoki.utsunomiya@oracle.com>
2026-01-16 13:03:06 +0900

    modified

--- startup.sh
+++ startup.sh
@@ -12,13 +12,13 @@ echo '<!DOCTYPE html>
 <body>
 <h1>This is a demo page</h1>
 </body>
-</html>' | sudo tee/usr/share/nginx/html/demo.html
+</html>' | sudo tee /usr/share/nginx/html/demo.html
 
 #ポート8080用に設定ファイル編集（/etc/nginx/nginx.conf）
 sudo sed -i 's/listen       80;/listen       8080;/' /etc/nginx/nginx.conf
 
 #デフォルトでindexにdemo.htmlが含まれるよう編集
-sudo sed -i 's/index  index.html index.htm;/index  demo.html index.html index.htm;/'/etc/nginx/nginx.conf
+sudo sed -i 's/index  index.html index.htm;/index  demo.html index.html index.htm;/' /etc/nginx/nginx.conf
 
 # nginx自動起動＆起動
 sudo systemctl enable nginx
@@ -26,4 +26,4 @@ sudo systemctl restart nginx
 
 # firewalldを開放
 sudo firewall-cmd --permanent --add-port=8080/tcp
-sudo firewall-cmd--reload
+sudo firewall-cmd --reload
